c84e57a48a3628af05230da32a22fde3ccba51e9
Oleksandr Kosii <oleksandr.kosii@valtech.com>
2023-10-10 15:55:45 +0300

    change to .net 7

--- Maui_Flower_App/Platforms/Android/MainApplication.cs
+++ Maui_Flower_App/Platforms/Android/MainApplication.cs
@@ -4,6 +4,8 @@ using Android.Runtime;
 namespace Maui_Flower_App;
 
 [Application]
+[MetaData("com.google.android.maps.v2.API_KEY",
+            Value = "AIzaSyAEbGVyi4u2aNYLbq5wNSk9SCduZVI0bfk")]
 public class MainApplication : MauiApplication
 {
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
